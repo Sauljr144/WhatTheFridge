68be0ae094dddb317fd7cfcceaebe0b3f0982ec1
AmirSmith413 <amirsmith166@gmail.com>
2023-11-30 21:16:56 -0800

    deleted help.js

--- app/screens/CreateAccountScreen.js
+++ app/screens/CreateAccountScreen.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {View,StyleSheet} from 'react-native'
+import { TextInput,Button } from 'react-native'
 
 const CreateAccountScreen = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
   return (
-    <div>
-        
-    </div>
+    <View style={styles.container}>
+
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    
+})
 
 export default CreateAccountScreen
