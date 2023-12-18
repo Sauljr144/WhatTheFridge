0559770934203b9396c3668338c69ae7181ede89
Valerie4792 <vaguilar4792@gmail.com>
2023-12-17 21:00:34 -0800

    added some styles to customDropDown.js

--- app/components/CustomDropDown.js
+++ app/components/CustomDropDown.js
@@ -1,8 +1,9 @@
 // CustomDropdown.js
 
 import React, { useState } from 'react';
-import { Modal, ModalBackdrop, ModalContent, Button, ButtonText } from '@gluestack-ui/themed';
+import { Modal, ModalBackdrop, ModalContent, Button, ButtonText, View } from '@gluestack-ui/themed';
 import { StyleSheet } from "react-native";
+
 const CustomDropdown = ({ items, onSelectItem }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
@@ -15,31 +16,52 @@ const CustomDropdown = ({ items, onSelectItem }) => {
 
   return (
     <>
-      <Button onPress={() => setIsOpen(true)} style={{ backgroundColor: 'white', borderColor: 'gray', borderWidth: 1 }}>
-        <ButtonText style={{ color: 'grey' }}>{selectedItem || 'Category'}</ButtonText>
+      <Button onPress={() => setIsOpen(true)} style={styles.categorySelect}>
+        <ButtonText style={{ color: 'grey', textAlign: 'left'  }}>{selectedItem || 'Category'}</ButtonText>
       </Button>
 
       {isOpen && (
         <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
           <ModalBackdrop />
 
-          <ModalContent style={styles.categorySelect}>
-            {items.map((item) => (
-              <Button key={item.value} onPress={() => handleSelect(item)} style={{ backgroundColor: 'white', borderColor: 'gray', borderWidth: 1 }}>
-                <ButtonText style={{ color: 'black' }}>{item.label}</ButtonText>
-              </Button>
-            ))}
+          <ModalContent style={styles.modalContent}>
+            <View>
+              {items.map((item) => (
+                <Button key={item.value} onPress={() => handleSelect(item)} style={styles.itemButton}>
+                  <ButtonText style={{ color: 'black'}}>{item.label}</ButtonText>
+                </Button>
+              ))}
+            </View>
           </ModalContent>
         </Modal>
       )}
     </>
   );
 };
+
 const styles = StyleSheet.create({
+  categorySelect: {
     backgroundColor: "white",
     margin: 10,
-    borderRadius: 10, // This will round the edges
+    borderRadius: 10,
     borderColor: 'transparent',
-    }
-)
+    padding: 10,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  itemButton: {
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 5,
+    padding: 10,
+  },
+});
+
 export default CustomDropdown;
