7c107d92b4f7bd8a9560b14eb4354f4ec4ea1f40
Valerie4792 <vaguilar4792@gmail.com>
2023-12-31 19:29:18 -0800

    changed from center to start

--- App.js
+++ App.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
+    
   },
 });
