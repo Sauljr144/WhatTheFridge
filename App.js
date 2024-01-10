07b235004de4792f445f611ce34056879a0c652e
Valerie4792 <vaguilar4792@gmail.com>
2024-01-09 21:10:44 -0800

    deleted FridgListScreenfrom AppJS

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ export default function App() {
     <GestureHandlerRootView>
       <Screen>
    <ShoppingListScreen/>
-   {/* <FridgeListScreen/> */}
   </Screen> 
     </GestureHandlerRootView>
 </GluestackUIProvider>
