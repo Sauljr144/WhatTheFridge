8aa5e79f6221f6f46358ebd26084dcdb33eae2d8
Valerie4792 <vaguilar4792@gmail.com>
2024-03-06 20:56:15 -0800

    deleted duplicates

--- app/screens/FridgeListScreen.js
+++ app/screens/FridgeListScreen.js
@@ -237,7 +237,7 @@ const FridgeListScreen = ({navigation}) => {
         ))}
       </ScrollView>
       <NavPiece navigation={navigation}/>
-    </>
+    
 
       {itemsToDisplay.map((item, index) => (
         <SwipableFridgeItem
